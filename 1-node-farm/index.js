db2addf88773caa703435a5ccbc51f85bb357e8f
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-02-06 07:59:41 +0000

    Routing

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path/posix');
+const url = require('url');
 
 ///////////////////////////////////////
 // Files
@@ -34,7 +36,23 @@ const http = require('http');
 ///////////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
-  res.end('Hello from the other side!');
+  console.log(req.url);
+
+  const pathName = req.url;
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the OVERVIEW');
+  } else if (pathName === '/product') {
+    res.end('This is the PRODUCT');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<ht>Page not found!</h1>');
+  }
+
+  // res.end('Hello from the other side!');
 });
 
 server.listen(3000, '127.0.0.1', () => {
