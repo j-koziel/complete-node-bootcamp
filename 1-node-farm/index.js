19686dc67080e6c45e559b13d27e5c2ae6dc7aa1
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-02-27 09:57:45 +0000

    learning more about npm

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -4,6 +4,8 @@ const path = require('path/posix');
 const { json } = require('stream/consumers');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////////////
@@ -54,6 +56,9 @@ const tempOverview = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
