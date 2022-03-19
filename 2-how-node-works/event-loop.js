ffd6b62bafcadb47f6d26cf36752f475909807a7
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-03-19 18:26:32 +0000

    The Event loop in practice

---
+++ 2-how-node-works/event-loop.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const crypto = require('crypto');
+
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 4;
+
+fs.readFile('test-file.txt', () => {
+  console.log('I/O finished');
+  console.log('---------------');
+
+  setTimeout(() => {
+    console.log('Timer 2 finished');
+  }, 0);
+  setTimeout(() => {
+    console.log('Timer 3 finished');
+  }, 3000);
+
+  setImmediate(() => console.log('Immediate 2 finished'));
+
+  process.nextTick(() => {
+    console.log('process.nextTick');
+  });
+
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+  crypto.pbkdf2Sync('password', 'salt', 100000, 1024, 'sha512');
+  console.log(Date.now() - start, 'Password encrypted');
+});
+
+console.log('Hello from the top level code');
