7b15076ccdc9a3bae51094de5da1b6d753815de9
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-03-20 06:58:13 +0000

    Events in practice

---
+++ 2-how-node-works/events.js
@@ -0,0 +1,46 @@
+const EventEmitter = require('events');
+const http = require('http');
+
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
+
+myEmitter.on('newSale', () => {
+  console.log('There was a new sale!');
+});
+
+myEmitter.on('newSale', () => {
+  console.log('Customer name: Jonas');
+});
+
+myEmitter.on('newSale', stock => {
+  console.log(`There are now ${stock} items left in stock`);
+});
+
+myEmitter.emit('newSale', 9);
+
+//////////////////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received!');
+  console.log(req.url);
+  res.end('Request received');
+});
+
+server.on('request', (req, res) => {
+  console.log('Another request');
+});
+
+server.on('close', () => {
+  res.end('server closed');
+});
+
+server.listen(8000, 'localhost', () => {
+  console.log('Waiting for requests...');
+});
