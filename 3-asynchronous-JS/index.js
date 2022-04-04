5de7a4f4528260418b1c24c495446fc672691d96
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-04 14:53:29 +0100

    excluding node_modules + callback hell

---
+++ 3-asynchronous-JS/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
+      if (err) return console.log(err.message);
+      console.log(res.body.message);
+
+      fs.writeFile('dog-img.txt', res.body.message, err => {
+        console.log('Random dog image saved to file!');
+      });
+    });
+});
