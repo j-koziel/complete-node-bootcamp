f1afe158f1b7b16ed9d3fcbd1b8a28d4b0c9b728
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-08 12:05:52 +0100

    Creating and parsing variables in a URL

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -31,6 +31,29 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+// parsing URL parameters
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = +req.params.id;
+
+  const tour = tours.find(el => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
 
