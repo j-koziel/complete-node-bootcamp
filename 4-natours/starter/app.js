68c68d7f3eedb8cc0c9edc03fbedc44b9491153e
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-08 10:46:05 +0100

    starting the api: handling GET requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,15 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res
-    .status(404)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(404)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+// Reading data
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`)
+);
+
+// Route handler
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
