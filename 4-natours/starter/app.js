39312df7468d5d36fc4162a68f147813afa00841
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-09 09:42:15 +0100

    Handling DELETE requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -89,6 +89,18 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
