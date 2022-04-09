7f328ec8b178d70b474f09193bcd05b626a8a2a3
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-09 09:37:59 +0100

    Handling PATCH requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -75,6 +75,20 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
