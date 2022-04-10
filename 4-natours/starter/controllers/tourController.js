61429b91988ad0514d6b293ccb8ac3aa06c8f3ed
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-10 10:07:57 +0100

    Implementing Param middleware

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -5,6 +5,16 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`${val}`);
+  if (+req.params.id > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  next();
+};
+
 exports.getAllTours = (req, res, next) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -18,22 +28,10 @@ exports.getAllTours = (req, res, next) => {
 };
 
 exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = +req.params.id;
-
-  const tour = tours.find(el => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
-      tour,
+      tours,
     },
   });
 };
@@ -60,11 +58,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (+req.params.id > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
   res.status(200).json({
     status: 'success',
     data: {
@@ -74,11 +67,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (+req.params.id > tours.length)
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
   res.status(204).json({
     status: 'success',
     data: null,
