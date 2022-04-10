831b31df6d912ddf79f686fcdb785290ee9d0300
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-10 13:21:53 +0100

    Chaining middleware

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -15,6 +15,16 @@ exports.checkID = (req, res, next, val) => {
   next();
 };
 
+// Check body function
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price)
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'Invalid request (400)' });
+
+  next();
+};
+
 exports.getAllTours = (req, res, next) => {
   console.log(req.requestTime);
   res.status(200).json({
--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -5,10 +5,15 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
