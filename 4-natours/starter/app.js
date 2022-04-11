c4511536b5e2ec8f9c074bb98df7deaf04bb1a72
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-11 10:16:34 +0100

    Serving static files

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,8 +8,8 @@ const app = express();
 
 // 1) Middleware
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
