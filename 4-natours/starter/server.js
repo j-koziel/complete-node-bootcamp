a75bf2e97f347a4be524c86c6d227dc0baa3d938
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-12 10:15:56 +0100

    Creating simple tour model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -19,6 +19,20 @@ mongoose
   })
   .then(() => console.log('DB connection established!'));
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: { type: Number, default: 4.5 },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
