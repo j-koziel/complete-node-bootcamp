fc428ee680dc53cb7d79ccac7c8795104ee69c38
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-12 10:26:05 +0100

    Creating documents and testing the model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -33,6 +33,20 @@ const tourSchema = new mongoose.Schema({
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log(`ERROR: ${err}`);
+  });
+
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
