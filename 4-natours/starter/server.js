80239853b09f03edecea1ded25d4dfca5dab61e0
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-12 09:56:17 +0100

    Connecting to the mongo DB with mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,10 +1,23 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection established!'));
 
 // Start server
 const port = process.env.PORT || 3000;
