8f6c6da0ce0e58bcc193f0024ee2286a03025dfc
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-15 08:44:04 +0100

    Adding limiting fields

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -26,6 +26,15 @@ exports.getAllTours = async (req, res, next) => {
       query = query.sort('-createdAt');
     }
 
+    // 3) Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+    console.log();
+
     // EXECUTE QUERY
     const tours = await query;
 
