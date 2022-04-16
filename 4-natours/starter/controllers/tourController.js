ee65445b105b3d2cc4f064d60978f87f89e91c7f
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-04-16 18:27:10 +0100

    Implementing pagination into the API

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -22,9 +22,10 @@ exports.getAllTours = async (req, res, next) => {
     if (req.query.sort) {
       const sortBy = req.query.sort.split(',').join(' ');
       query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
     }
+    // else {
+    //   query = query.sort('-createdAt');
+    // }
 
     // 3) Field Limiting
     if (req.query.fields) {
@@ -33,16 +34,22 @@ exports.getAllTours = async (req, res, next) => {
     } else {
       query = query.select('-__v');
     }
-    console.log();
+
+    // 4) Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
 
     // EXECUTE QUERY
     const tours = await query;
 
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('diffic ulty')
-    //   .equals('easy');
     // SEND RESPONSE
     res.status(200).json({
       status: 'success',
