c60c8bf51ea2d0cf42368d6e6c318c575a7555b5
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-05-01 09:19:07 +0100

    aggregation middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const { defaultConfiguration } = require('../app');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -98,7 +97,13 @@ tourSchema.pre(/^find/, function (next) {
 
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
-  console.log(docs);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
