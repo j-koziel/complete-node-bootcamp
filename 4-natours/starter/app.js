bd01acd7b6e8185d206a2f2cbf771da89a9919b2
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-05-24 19:51:27 +0100

    Implementing global error handling middleware

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -25,9 +25,26 @@ app.use('/api/v1/users', userRouter);
 
 // Handling all unhandled routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
