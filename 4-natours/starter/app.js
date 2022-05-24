61f4a083b330a38b35fb7d65b035aa607ea7272a
Jonathan Koziel <68497216+programmer-boi@users.noreply.github.com>
2022-05-24 19:31:57 +0100

    Handling unhandled routes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -15,11 +15,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -27,5 +22,13 @@ app.use((req, res, next) => {
 // 3) Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// Handling all unhandled routes
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
