1814da4569ef337a70a7a61fbd5035faff006fad
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-07-28 09:12:59 +0100

    Handling uncaught exceptions

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -24,9 +30,9 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on('uncaughtException', err => {
-  console.log(err.name, err.message);
+process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
