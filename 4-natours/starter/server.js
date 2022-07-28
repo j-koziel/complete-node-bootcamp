f3c90722fd963f4579ccb78fa2814a37a7d1fafc
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-07-28 08:52:07 +0100

    Handling unhandled rejections

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -20,6 +20,14 @@ mongoose
 
 // Start server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
