0d60ddcdcdfd8dba5c4273b9aec4f1bbea6ce084
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-07-29 09:38:45 +0100

    Signing up users (using jwt)

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -1,11 +1,24 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     staus: 'success',
+    token,
     data: {
       user: newUser,
     },
