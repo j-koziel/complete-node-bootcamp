a3fce1eb1efa596141636d85de17e628dc779cf7
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-08-07 09:15:39 +0100

    Sending emails with nodemailer
    Also got to use mailtrap which traps emails instead of sending them to the actual email addresses

---
+++ 4-natours/starter/utils/email.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const sendEmail = async options => {
+  // 1) Create a transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  // 2) Define the email options
+  const mailOptions = {
+    from: 'Jonathan Koziel<3 <tweggy4@gmail.com>',
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  };
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
