a7dda902f3dac8094c1cc2f4d8ba314861241ef5
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-08-07 08:42:28 +0100

    Creating a reset token to reset password
    Creating a token to allow the user to reset their password. Two versions of this are created. An encrypted one which is stored in the database and an unencrypted one which is sent to the user.

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -101,3 +101,17 @@ exports.restrictTo =
 
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with that email address.', 404));
+  }
+
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+  // 3) Send it to user's email
+});
+exports.resetPassword = (req, res, next) => {};
