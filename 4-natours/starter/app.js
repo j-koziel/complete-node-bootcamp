96f7d05698330b4e6ea7b6686de5f110f7d37cdf
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-09-20 14:51:41 +0100

    Implementing rate limiting.
    Using the express-rate-limit npm package to add rate limiting to the app. This will prevent ddos attacks so that too many requests are not sent to the server and cause it to slow down and eventually crash.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -12,6 +13,13 @@ const app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 5,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
