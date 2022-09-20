1841072ea914e16438d6389b3c529ed63c4e86ce
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-09-20 15:12:49 +0100

    Setting security HTTP headers.
    Using the helmet npm package to set some security HTTP headers which the browser can then act upon. Also cleaning up the middleware stack in my code with some comments

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,10 +11,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) Middleware
+// Set secuirty http headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests to the same API
 const limiter = rateLimit({
   max: 5,
   windowMs: 60 * 60 * 1000,
@@ -21,9 +27,17 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
