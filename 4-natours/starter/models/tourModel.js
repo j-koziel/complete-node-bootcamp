a26164042a77b635c2c1ae1312dc8c5e05f3122b
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-09-26 20:51:50 +0100

    Populating Tour Guides
    Populating the referenced tour guides so that all though id referencing is still being used it looks like the dataset is embedded into the tour

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -139,6 +139,11 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
+tourSchema.pre(/^find/, function (next) {
+  this.populate({ path: "guides", select: "-__v -passwordChangedAt" });
+  next();
+});
+
 tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
   next();
