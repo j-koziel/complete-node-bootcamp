a48e45172f2b12919d06e603fde2d70894e7c757
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-10-04 11:16:55 +0100

    Populating the reviews
    Defining a pre hook on the review model and calling the populate method to display the tour and user which is referenced in the review document

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -25,6 +25,15 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+// QUERY MIDDLEWARE
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({ path: "tour", select: "name" }).populate({
+    path: "user",
+    select: "name photo",
+  });
+  next();
+});
+
 const Review = mongoose.model("Review", reviewSchema);
 
 module.exports = Review;
