099817f38c0ff7a76f33999cf24826a4a645d671
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-11-11 19:46:18 +0000

    Creating a current user getter handler

--- 4-natours/starter/controllers/userController.js
+++ 4-natours/starter/controllers/userController.js
@@ -38,6 +38,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.deleteMe = catchAsync(async (req, res, next) => {
   await User.findByIdAndUpdate(req.user.id, { active: false });
 
