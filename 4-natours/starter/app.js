ebecb7a90f2401fdb6525273c4aff4b7c4c2f7c5
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-12-12 08:59:17 +0000

    Setting up pug for express.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
@@ -13,6 +14,12 @@ const userRouter = require("./routes/userRoutes");
 const reviewRouter = require("./routes/reviewRoutes");
 
 const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+// Serving static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // 1) Middleware
 // Set secuirty http headers
@@ -58,9 +65,6 @@ app.use(
   })
 );
 
-// Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -68,6 +72,10 @@ app.use((req, res, next) => {
 });
 
 // 3) Routes
+app.get("/", (req, res) => {
+  res.status(200).render("base");
+});
+
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/reviews", reviewRouter);
