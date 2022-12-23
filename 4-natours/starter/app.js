be1a61c869cc915a1556f4c36a2b06113468a5e3
Jonathan Koziel <jonathankoziel1@gmail.com>
2022-12-23 13:01:55 +0000

    First steps with pug.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 
 // 3) Routes
 app.get("/", (req, res) => {
-  res.status(200).render("base");
+  res.status(200).render("base", { tour: "The Forest Hiker", user: "Jonas" });
 });
 
 app.use("/api/v1/tours", tourRouter);
